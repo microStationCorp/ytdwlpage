289c7bff7c3ce6a9ef88cfe437b059254ab1b582
microStationCorp <sujanmondal916@gmail.com>
2020-01-17 17:44:19 +0530

    design change and optimesed

--- youtubeDwnld/views.py
+++ youtubeDwnld/views.py
@@ -5,36 +5,71 @@ import youtube_dl
 
 
 def home(request):
-    return render(request, 'index.html')
-
-
-# def crawl(request):
-#     url = request.GET.get('urlBox')
-#     video = pafy.new(url)
-#     strm = video.streams
-#     html = []
-#     for s in strm:
-#         html.append({
-#             'res': f"{s.resolution}",
-#             'link': f"{s.url}"
-#         })
-#     return render(request, 'crawling.html', {'url': html})
-
-def crawl(request):
     URL = request.GET.get('urlBox')
-    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+    if URL != None:
+        try:
+            ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
 
-    with ydl:
-        result = ydl.extract_info(
-            URL,
-            download=False  # We just want to extract the info
-        )
-    formats = result['formats']
-    html = []
+            with ydl:
+                result = ydl.extract_info(
+                    URL,
+                    download=False  # We just want to extract the info
+                )
+            if 'entries' in result:
+                return render(request, 'index.html', {'mflag': 'True'})
+            html = dict()
+            html.update({
+                'uploader': f"{result['uploader']}",
+                'title': f"{result['title']}",
+                'thumbnail': result['thumbnail'],
+                'description': f"{result['description']}"
+            })
+            formats = result['formats']
+            html.update({
+                'formats': {
+                    'video': [],
+                    'videoWoAudio': [],
+                    'audio': [],
+                    'others': []
+                }
+            })
 
-    for item in formats:
-        html.append({
-            'format': f"{item['format']}",
-            'url': f"{item['url']}"
-        })
-    return render(request, 'crawling.html', {'url': html, 'thnl': result['thumbnail'], 'title': result['title']})
+            for format in formats:
+                if format['height'] != None and format['width'] != None:
+                    if format['asr'] == None and format['fps'] != None:
+                        html['formats']['videoWoAudio'].append({
+                            'ext': f"{format['ext']}",
+                            'format': f"{format['format']}",
+                            'url': f"{format['url']}",
+                            'asr': f"{format['asr']}",
+                            'fps': f"{format['fps']}"
+                        })
+                    else:
+                        html['formats']['video'].append({
+                            'ext': f"{format['ext']}",
+                            'format': f"{format['format']}",
+                            'url': f"{format['url']}",
+                            'asr': f"{format['asr']}",
+                            'fps': f"{format['fps']}"
+                        })
+                elif format['height'] == None and format['width'] == None:
+                    html['formats']['audio'].append({
+                        'ext': f"{format['ext']}",
+                        'format': f"{format['format']}",
+                        'url': f"{format['url']}",
+                        'asr': f"{format['asr']}",
+                        'fps': f"{format['fps']}"
+                    })
+                else:
+                    html['formats']['others'].append({
+                        'ext': f"{format['ext']}",
+                        'format': f"{format['format']}",
+                        'url': f"{format['url']}",
+                        'asr': f"{format['asr']}",
+                        'fps': f"{format['fps']}"
+                    })
+            return render(request, 'index.html', {'aflag': 'True', 'dataDict': html})
+        except:
+            return render(request, 'index.html', {'eflag': 'True'})
+    else:
+        return render(request, 'index.html')
