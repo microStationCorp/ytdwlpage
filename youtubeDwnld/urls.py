9d993ad09429c5945d3abe8c9a7a139b15a1f7e1
microStationCorp <sujanmondal916@gmail.com>
2020-02-01 17:03:47 +0530

    nothing

--- youtubeDwnld/urls.py
+++ youtubeDwnld/urls.py
@@ -18,7 +18,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('dmca/', views.dmca, name='dmca'),
     path('contactUs/', views.cu, name='contactUs'),
